c1e69aca75f88be2c8eb9d20a59a65eb5f584539
Suraj Ghimire <suraj@ghishadow.com>
2023-07-31 07:26:51 +0530

    fix buildflags, add fenix for rust
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }:
+{pkgs, ...}: {
+  packages = with pkgs; [git openssl cmake cargo-audit rustup cargo-cache cargo-dist cargo-insta];
 
-{
-
-  packages = with pkgs; [ git openssl cmake cargo-audit rustup cargo-cache cargo-dist cargo-insta ];
+  difftastic.enable = true;
 
   languages.nix.enable = true;
-  languages.rust.enable = true;
+  languages.rust = {
+    enable = true;
+  };
 
+  pre-commit.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
 }
