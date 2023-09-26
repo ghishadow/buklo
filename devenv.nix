d572a0b00663dc342c95dcfb35d4670aa401c4bf
Suraj Ghimire <suraj@ghishadow.com>
2023-09-26 10:40:45 +0530

    switch to python rewrite of devenv
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,28 @@
 {pkgs, ...}: {
-  packages = with pkgs; [git openssl cmake cargo-audit rustup cargo-cache cargo-dist cargo-insta];
+  env.CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG=true;
+  env.LC_ALL="en_US.UTF-8";
+  packages = with pkgs; [
+    git
+    openssl
+    cmake
+    cargo-audit
+    rustup
+    cargo-cache
+    cargo-dist
+    cargo-insta
+    cargo-audit
+    cargo-auditable
+    clang_16
+    llvmPackages_16.stdenv
+    lld_16
+    rust-analyzer
+    nil
+    zoxide
+    ];
 
   difftastic.enable = true;
 
   languages.nix.enable = true;
-  languages.rust = {
-    enable = true;
-  };
 
   pre-commit.hooks = {
     clippy.enable = true;
